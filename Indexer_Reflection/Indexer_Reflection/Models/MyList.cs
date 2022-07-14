f82b5784c90d6d89a983340d88a911d29845ab11
SadiqNahmetov <sadigfn@code.edu.az>
2022-07-14 07:13:44 +0400

    Homework Indexer

---
+++ Indexer_Reflection/Indexer_Reflection/Models/MyList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomAddedMethod.Models
+{
+    public class MyLIst<T>
+    {
+        public string[] arr;
+
+        public MyLIst()
+        {
+            arr = new string[0];
+        }
+
+        public void Add(string names)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = names;
+        }
+
+        public void Clear(string names)
+        {
+            foreach (var item in arr)
+            {
+                if (item != names)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+        }
+
+
+        public void GetAll()
+        {
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
---
+++ Indexer_Reflection/Indexer_Reflection/Program.cs
@@ -0,0 +1,28 @@
+﻿using CustomAddedMethod.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CustomAddedMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLIst<string> teacherList = new MyLIst<string>();
+
+            teacherList.Add("Cavid");
+            teacherList.Add("Sadiq");
+            teacherList.Add("Esqin");
+            teacherList.Add("Ramil");
+
+
+            teacherList.Clear("Esqin");
+
+            //teacherList.Find("Ramil");
+
+
+
+        }
+    }
+
+}
